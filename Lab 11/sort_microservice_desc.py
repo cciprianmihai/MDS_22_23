7ec1b94ff99ccef12580d1c26251998d20ad194a
Ciprian Mihai Ceausescu <ciprian.mihai.ceausescu@gmail.com>
2023-05-07 12:02:51 +0300

    Add files via upload

---
+++ Lab 11/sort_microservice_desc.py
@@ -0,0 +1,17 @@
+from flask import Flask, request
+import requests
+
+app = Flask(__name__)
+
+@app.route('/sort', methods=['POST'])
+def sort_vector():
+    data = request.get_json()
+    vector = data.get('vector')
+    if vector:
+        sorted_vector = sorted(vector, reverse=True)
+        return {'sorted_vector': sorted_vector}
+    else:
+        return {'error': 'Invalid data'}, 400
+
+if __name__ == '__main__':
+    app.run(port=5001)
